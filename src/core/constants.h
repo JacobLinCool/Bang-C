e3a131602ef27e30d162b4bcbfeb962821f21ef0
Takala <dylan91011028@gmail.com>
2022-06-12 10:28:24 +0000

    complete some card function

--- src/core/constants.h
+++ src/core/constants.h
@@ -7,4 +7,10 @@
 #define FAIL 0
 #define SUCCESS 1
 
+char* role_name[4] = {"Sheriff", "Deputy", "Criminal", "Traitor"};
+char* character_name[16] = {"Bart_Cassidy",    "Black_Jack",     "Calamity_Janet", "El_Gringo",
+                            "Jesse_Jones",     "Jourdonnais",    "Kit_Carlson",    "Lucky_Duke",
+                            "Paul_Regret",     "Pedro_Ramirez",  "Rose_Doolan",    "Sid_Ketchum",
+                            "Slab_the_Killer", "Suzy_Lafayette", "Vulture_Sam",    "Willy_the_Kid"};
+
 #endif  // __CORE_CONSTANTS_H
