fc1b58f273a658e9d2f666b4415c63e356d4ce62
JacobLinCool <jacoblincool@gmail.com>
2022-06-12 22:58:55 +0800

    feat: add card names as global strings

--- src/core/constants.h
+++ src/core/constants.h
@@ -7,10 +7,31 @@
 #define FAIL 0
 #define SUCCESS 1
 
-char* role_name[4] = {"Sheriff", "Deputy", "Criminal", "Traitor"};
-char* character_name[16] = {"Bart_Cassidy",    "Black_Jack",     "Calamity_Janet", "El_Gringo",
-                            "Jesse_Jones",     "Jourdonnais",    "Kit_Carlson",    "Lucky_Duke",
-                            "Paul_Regret",     "Pedro_Ramirez",  "Rose_Doolan",    "Sid_Ketchum",
-                            "Slab_the_Killer", "Suzy_Lafayette", "Vulture_Sam",    "Willy_the_Kid"};
+char* role_name[] = {"", "Sheriff", "Deputy", "Criminal", "Traitor"};
+char* character_name[] = {"",
+                          "Bart_Cassidy",
+                          "Black_Jack",
+                          "Calamity_Janet",
+                          "El_Gringo",
+                          "Jesse_Jones",
+                          "Jourdonnais",
+                          "Kit_Carlson",
+                          "Lucky_Duke",
+                          "Paul_Regret",
+                          "Pedro_Ramirez",
+                          "Rose_Doolan",
+                          "Sid_Ketchum",
+                          "Slab_the_Killer",
+                          "Suzy_Lafayette",
+                          "Vulture_Sam",
+                          "Willy_the_Kid"};
+
+char* card_name[] = {"Hidden",    "Bang",         "Missed",     "Gatling",     "Indians",
+                     "Panic",     "Cat Balou",    "Stagecoach", "Wells Fargo", "General Store",
+                     "Beer",      "Saloon",       "Duel",       "Barrel",      "Scope",
+                     "Mustang",   "Jail",         "Dynamite",   "Volcanic",    "Schofield",
+                     "Remington", "Rev Carabine", "Winchester"
+
+};
 
 #endif  // __CORE_CONSTANTS_H
