64e34c62f08e412e71d3ef9280f5eeacfe0a18c3
JacobLinCool <jacoblincool@gmail.com>
2022-06-15 01:02:18 +0800

    feat: ws server and message queue

--- src/web/server.c
+++ src/web/server.c
@@ -1,283 +1,217 @@
+#include <libwebsockets.h>
 #include <unistd.h>
 
 #include "../third/cJSON/cJSON.h"
 #include "../third/cimple/all.h"
+#include "../third/uds/deque.h"
 #include "../third/uds/vector.h"
-#include "../third/wsServer/include/ws.h"
 #include "../utils/all.h"
+#define BUFFER_SIZE (64 * 1024)
 
-StructVector(Connections, ws_cli_conn_t *, NULL);
+typedef struct Message {
+    char  *payload;
+    size_t len;
+} Message;
+StructDeque(Messages, Message *, NULL);
 
-typedef struct NamedConnection {
-    ws_cli_conn_t *client;
-    char          *name;
-} NamedConnection;
+typedef struct Client {
+    struct lws *instance;
+    char        name[256];
+    Messages   *msg_queue;
+} Client;
+StructVector(Clients, Client *, NULL);
 
-StructVector(NamedConnections, NamedConnection *, NULL);
+Clients *clients = NULL;
 
-Connections      *connections = NULL;
-NamedConnections *named_connections = NULL;
-
-/**
- * @brief Send a JSON message with a given type and payload. Payload will be freed after sending.
- *
- * @param client
- * @param type
- * @param payload
- * @return int
- */
-int send_message(ws_cli_conn_t *client, const char *type, cJSON *payload) {
-    cJSON *obj = cJSON_CreateObject();
-    cJSON_AddItemToObject(obj, "type", cJSON_CreateStringReference(type));
-    cJSON_AddItemToObject(obj, "payload", payload);
-
-    char *json = cJSON_PrintUnformatted(obj);
-
-    int length = ws_sendframe_txt(client, json);
-
-    free(json);
-    cJSON_Delete(obj);
-
-    return length;
+static void free_message(Message *msg) {
+    free(msg->payload);
+    msg->payload = NULL;
+    msg->len = 0;
 }
 
-/**
- * @brief Broadcast a message to all clients.
- *
- * @param type
- * @param payload
- */
-void broadcast(const char *type, cJSON *payload) {
-    cJSON *obj = cJSON_CreateObject();
-    cJSON_AddItemToObject(obj, "type", cJSON_CreateStringReference(type));
-    cJSON_AddItemToObject(obj, "payload", payload);
-
-    char *json = cJSON_PrintUnformatted(obj);
-
-    for (int i = 0; i < named_connections->size; i++) {
-        ws_sendframe_txt(named_connections->get(named_connections, i)->client, json);
-    }
-
-    free(json);
-    cJSON_Delete(obj);
-}
-
-void onopen(ws_cli_conn_t *client) {
-    connections->push(connections, client);
-
-    char *address = ws_getaddress(client);
-    Console.yellow("Connection opened, address: %s", address);
-
-    cJSON *obj = cJSON_CreateObject();
-    cJSON_AddItemToObject(obj, "question", cJSON_CreateStringReference("What's your name?"));
-
-    send_message(client, "ask", obj);
-}
-
-void onclose(ws_cli_conn_t *client) {
-    for (size_t i = 0; i < connections->size; i++) {
-        if (connections->get(connections, i) == client) {
-            connections->remove(connections, i);
-            break;
+Client *find_client(struct lws *instance) {
+    for (int i = 0; i < clients->size; i++) {
+        Client *client = clients->data[i];
+        if (client->instance == instance) {
+            return client;
         }
     }
-
-    for (size_t i = 0; i < named_connections->size; i++) {
-        if (named_connections->get(named_connections, i)->client == client) {
-            NamedConnection *nc = named_connections->remove(named_connections, i);
-
-            cJSON *obj = cJSON_CreateObject();
-            cJSON_AddItemToObject(obj, "name", cJSON_CreateStringReference(nc->name));
-
-            broadcast("leave", obj);
-            Console.magenta("%s Left!", nc->name);
-            free(nc->name);
-            break;
-        }
-    }
-
-    char *address = ws_getaddress(client);
-    Console.yellow("Connection closed, address: %s", address);
+    return NULL;
 }
 
-void onmessage(ws_cli_conn_t *client, const unsigned char *msg, uint64_t size, int type) {
-    NamedConnection *sender = NULL;
+void start_server(int port, lws_callback_function callback) {
+    struct lws_protocols protocols[] = {{.id = 2048,
+                                         .name = "cool-protocol",
+                                         .callback = callback,
+                                         .per_session_data_size = sizeof(Client *),
+                                         .rx_buffer_size = BUFFER_SIZE,
+                                         .user = NULL,
+                                         .tx_packet_size = 0},
+                                        LWS_PROTOCOL_LIST_TERM};
 
-    for (size_t i = 0; i < named_connections->size; i++) {
-        if (named_connections->get(named_connections, i)->client == client) {
-            sender = named_connections->get(named_connections, i);
-            break;
-        }
-    }
+    struct lws_context_creation_info info;
+    struct lws_context              *context;
 
-    if (msg == NULL) {
-        Console.red("Invalid msg");
+    memset(&info, 0, sizeof(info));
+
+    info.port = port;
+    info.protocols = protocols;
+    info.vhost_name = "localhost";
+
+    context = lws_create_context(&info);
+
+    if (context == NULL) {
+        Console.error("WebSocket context init failed");
         return;
     }
 
-    cJSON *json = cJSON_Parse((const char *)msg);
+    Console.info("WebSocket server started");
 
-    if (json == NULL) {
-        Console.red("Invalid JSON");
-        return;
+    while (lws_service(context, 0) >= 0) {
+        // nothing!
     }
 
-    cJSON *action_token = cJSON_GetObjectItem(json, "action");
-    char  *action = NULL;
-    if (cJSON_IsString(action_token) && (action_token->valuestring != NULL)) {
-        action = action_token->valuestring;
-    } else {
-        cJSON_Delete(json);
-        return;
+    lws_context_destroy(context);
+
+    Console.info("WebSocket server stopped");
+}
+
+void handle_action(Client *client, char *action, cJSON *payload) {
+    if (strcmp("stop", action) == 0) {
+        exit(0);
     }
 
-    // Console.blue("%s: %s", sender ? sender->name : "Unnamed", action);
+    Message *msg = malloc(sizeof(Message));
+    msg->payload = cJSON_PrintUnformatted(payload);
+    msg->len = strlen(msg->payload);
+    client->msg_queue->push(client->msg_queue, msg);
 
-    if (sender == NULL) {
-        if (strcmp(action, "name") == 0) {
-            cJSON *name_token = cJSON_GetObjectItem(json, "name");
-            char  *name = NULL;
+    lws_callback_on_writable(client->instance);
+}
 
-            if (cJSON_IsString(name_token) && name_token->valuestring != NULL) {
-                name = $(strdup(name_token->valuestring));
-            } else {
+static int event_handler(struct lws *instance, enum lws_callback_reasons reason, void *user,
+                         void *in, size_t len) {
+    Client *client = find_client(instance);
+
+    switch (reason) {
+        case LWS_CALLBACK_ESTABLISHED: {
+            if (clients->size >= 7) {
+                Console.red("[%p] Room is full, Connection refused", instance);
+                break;
+            }
+
+            Console.yellow("[%p] Connection established", instance);
+            client = calloc(1, sizeof(Client));
+            client->instance = instance;
+            client->msg_queue = create_Messages();
+            clients->push(clients, client);
+
+            break;
+        }
+
+        case LWS_CALLBACK_RECEIVE: {
+            if (client == NULL) {
+                Console.red("[%p] Client is NULL", instance);
+                break;
+            }
+
+            char  *data = strndup((char *)in, len);
+            cJSON *json = cJSON_Parse(data);
+            if (json == NULL) {
+                Console.red("[%p] Invalid JSON: %s", instance, data);
+                break;
+            }
+
+            cJSON *action_token = cJSON_GetObjectItem(json, "action");
+            char  *action = NULL;
+
+            if (cJSON_IsString(action_token) == false || (action_token->valuestring == NULL)) {
                 cJSON_Delete(json);
-                return;
+                break;
             }
 
-            if (memcmp(name, "SERVER", 6) == 0) {
-                cJSON *obj = cJSON_CreateObject();
-                cJSON_AddItemToObject(
-                    obj, "question",
-                    cJSON_CreateStringReference(
-                        "You can't take \"SERVER\" as your name. Use another one."));
-                send_message(client, "ask", obj);
+            action = action_token->valuestring;
 
-                free(name);
-                return;
+            cJSON *payload_token = cJSON_GetObjectItem(json, "payload");
+            cJSON *payload = NULL;
+
+            if (cJSON_IsObject(payload_token) == false) {
+                cJSON_Delete(json);
+                break;
             }
 
-            for (size_t i = 0; i < named_connections->size; i++) {
-                if (strcmp(named_connections->get(named_connections, i)->name, name) == 0) {
-                    cJSON *obj = cJSON_CreateObject();
-                    cJSON_AddItemToObject(obj, "question",
-                                          cJSON_CreateStringReference(
-                                              "This name is already taken. Use another one."));
-                    send_message(client, "ask", obj);
+            payload = payload_token;
 
-                    free(name);
-                    return;
+            Console.cyan("[%p] Received action: %s", instance, action);
+            handle_action(client, action, payload);
+
+            break;
+        }
+
+        case LWS_CALLBACK_SERVER_WRITEABLE: {
+            if (client == NULL) {
+                Console.red("[%p] Client is NULL", instance);
+                break;
+            }
+
+            if (client->msg_queue->size == 0) {
+                Console.blue("[%p] No messages to send", instance);
+                break;
+            }
+
+            Message *msg = client->msg_queue->shift(client->msg_queue);
+
+            if (msg == NULL) {
+                Console.red("[%p] Message is NULL", instance);
+                break;
+            }
+
+            Console.blue("[%p] Sending %s", instance, msg->payload);
+            lws_write(instance, msg->payload, msg->len, LWS_WRITE_TEXT);
+
+            // free_message(msg);
+            // free(msg);
+
+            if (client->msg_queue->size > 0) {
+                lws_callback_on_writable(instance);
+            }
+
+            break;
+        }
+
+        case LWS_CALLBACK_CLOSED: {
+            if (client == NULL) {
+                Console.red("[%p] Client is NULL", instance);
+                break;
+            }
+
+            Console.yellow("[%p] Connection closed", instance);
+            for (size_t i = 0; i < clients->size; i++) {
+                Client *client = clients->get(clients, i);
+                if (client->instance == instance) {
+                    clients->remove(clients, i);
                 }
             }
 
-            NamedConnection *nc = calloc(1, sizeof(NamedConnection));
-            nc->client = client;
-            nc->name = name;
-            named_connections->push(named_connections, nc);
-
-            cJSON *obj = cJSON_CreateObject();
-            cJSON_AddItemToObject(obj, "from", cJSON_CreateStringReference("SERVER"));
-            cJSON_AddItemToObject(obj, "to", cJSON_CreateStringReference(name));
-            cJSON_AddItemToObject(obj, "message",
-                                  cJSON_CreateStringReference($(String.format("Hello %s!", name))));
-
-            send_message(client, "chat", obj);
-
-            obj = cJSON_CreateObject();
-            cJSON *players = cJSON_CreateArray();
-            for (size_t i = 0; i < named_connections->size; i++) {
-                cJSON_AddItemToArray(
-                    players,
-                    cJSON_CreateString(named_connections->get(named_connections, i)->name));
+            while (client->msg_queue->size > 0) {
+                Message *msg = client->msg_queue->shift(client->msg_queue);
+                free_message(msg);
             }
-            cJSON_AddItemToObject(obj, "players", players);
-            send_message(client, "players", obj);
 
-            obj = cJSON_CreateObject();
-            cJSON_AddItemToObject(obj, "name", cJSON_CreateStringReference(name));
+            client->msg_queue->free(client->msg_queue);
+            free(client);
 
-            broadcast("join", obj);
-            Console.magenta("%s Joined!", name);
-        } else {
-            cJSON *obj = cJSON_CreateObject();
-            cJSON_AddItemToObject(obj, "question",
-                                  cJSON_CreateStringReference("What's your name?"));
-
-            send_message(client, "ask", obj);
-        }
-    } else if (strcmp(action, "chat") == 0) {
-        cJSON *message_token = cJSON_GetObjectItem(json, "message");
-        char  *message = NULL;
-
-        if (cJSON_IsString(message_token) && (message_token->valuestring != NULL)) {
-            message = message_token->valuestring;
-        } else {
-            cJSON_Delete(json);
-            return;
-        }
-
-        Console.cyan("Received [%s], from: %s", message, sender->name);
-
-        cJSON *to_token = cJSON_GetObjectItem(json, "to");
-        char  *to = NULL;
-
-        if (cJSON_IsString(to_token) && (to_token->valuestring != NULL)) {
-            to = to_token->valuestring;
-        } else {
-            cJSON_Delete(json);
-            return;
-        }
-
-        if (strcmp(to, "SERVER") == 0) {
-            for (size_t i = 0; i < named_connections->size; i++) {
-                NamedConnection *receiver = named_connections->get(named_connections, i);
-                cJSON           *obj = cJSON_CreateObject();
-                cJSON_AddItemToObject(obj, "from", cJSON_CreateString("SERVER"));
-                cJSON_AddItemToObject(obj, "to", cJSON_CreateString(receiver->name));
-                cJSON_AddItemToObject(obj, "message", cJSON_CreateString(message));
-
-                send_message(receiver->client, "chat", obj);
-            }
-        } else {
-            for (size_t i = 0; i < named_connections->size; i++) {
-                NamedConnection *receiver = named_connections->get(named_connections, i);
-                if (strcmp(receiver->name, to) == 0) {
-                    cJSON *obj = cJSON_CreateObject();
-                    cJSON_AddItemToObject(obj, "from", cJSON_CreateString(sender->name));
-                    cJSON_AddItemToObject(obj, "to", cJSON_CreateString(receiver->name));
-                    cJSON_AddItemToObject(obj, "message", cJSON_CreateString(message));
-
-                    send_message(receiver->client, "chat", obj);
-
-                    if (receiver->client != sender->client) {
-                        obj = cJSON_CreateObject();
-                        cJSON_AddItemToObject(obj, "from", cJSON_CreateString(sender->name));
-                        cJSON_AddItemToObject(obj, "to", cJSON_CreateString(receiver->name));
-                        cJSON_AddItemToObject(obj, "message", cJSON_CreateString(message));
-
-                        send_message(sender->client, "chat", obj);
-                    }
-
-                    break;
-                }
-            }
+            break;
         }
     }
 
-    cJSON_Delete(json);
+    return 0;
 }
 
 int main(void) {
-    connections = create_Connections();
-    named_connections = create_NamedConnections();
+    clients = create_Clients();
 
-    struct ws_events evt = {
-        .onopen = onopen,
-        .onclose = onclose,
-        .onmessage = onmessage,
-    };
-
-    ws_socket(&evt, 8080, 0, 1000);
+    start_server(8080, event_handler);
 
     return EXIT_SUCCESS;
 }
