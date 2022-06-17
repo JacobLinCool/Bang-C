a016db6c697ebe1d0d98dc84cd498647f064d8be
JacobLinCool <jacoblincool@gmail.com>
2022-06-17 21:36:38 +0800

    feat(client): Card component

---
+++ web/src/components/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { CardType } from "../types";
+import { t } from "../i18n";
+const props = defineProps({
+    type: { type: Number, default: 0 },
+    suit: { type: Number, default: 0 },
+    number: { type: Number, default: 0 },
+});
+
+const suits = ["", "♠", "♥", "♦", "♣"];
+</script>
+
+<template>
+    <div
+        :title="t(CardType[props.type])"
+        class="p-1t ransition-all duration-100 ease-in-out transform hover:scale-110 opacity-75 hover:opacity-100"
+    >
+        <img
+            class="w-full"
+            style="aspect-ratio: 9 / 16"
+            :src="`/card/${CardType[props.type]}.png`"
+        />
+        <div v-if="props.number" class="absolute bottom-[5%] left-[5%]">{{ props.number }}</div>
+        <div v-if="props.suit" class="absolute bottom-[5%] left-[17%]">
+            {{ suits[props.suit] }}
+        </div>
+    </div>
+</template>
