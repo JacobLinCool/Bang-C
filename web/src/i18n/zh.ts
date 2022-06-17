44a9c2f713671c84d48326be43905d9a1c6d2e86
JacobLinCool <jacoblincool@gmail.com>
2022-06-17 23:54:14 +0800

    feat: Chinese character names

--- web/src/i18n/zh.ts
+++ web/src/i18n/zh.ts
@@ -1,5 +1,5 @@
 /**
- * Chinease Display Names
+ * Chinese Display Names
  */
 export default {
     /** Roles */
@@ -9,22 +9,22 @@ export default {
     Traitor: "叛徒",
 
     /** Characters */
-    Bart_Cassidy: "Bart Cassidy",
-    Black_Jack: "Black Jack",
-    Calamity_Janet: "Calamity Janet",
-    El_Gringo: "El Gringo",
-    Jesse_Jones: "Jesse Jones",
-    Jourdonnais: "Jourdonnais",
-    Kit_Carlson: "Kit Carlson",
-    Lucky_Duke: "Lucky Duke",
-    Paul_Regret: "Paul Regret",
-    Pedro_Ramirez: "Pedro Ramirez",
-    Rose_Doolan: "Rose Doolan",
-    Sid_Ketchum: "Sid Ketchum",
-    Slab_the_Killer: "Slab the Killer",
-    Suzy_Lafayette: "Suzy Lafayette",
-    Vulture_Sam: "Vulture Sam",
-    Willy_the_Kid: "Willy the Kid",
+    Bart_Cassidy: "巴特・卡西迪",
+    Black_Jack: "黑傑克",
+    Calamity_Janet: "凶神賈奈特",
+    El_Gringo: "艾爾・格林哥",
+    Jesse_Jones: "傑西・瓊斯",
+    Jourdonnais: "喬多內",
+    Kit_Carlson: "工具人卡爾森",
+    Lucky_Duke: "幸運公爵",
+    Paul_Regret: "保羅・瑞格萊特",
+    Pedro_Ramirez: "保羅・列維爾",
+    Rose_Doolan: "蘿斯・杜蘭",
+    Sid_Ketchum: "席德・可臣",
+    Slab_the_Killer: "殺手史拉伯",
+    Suzy_Lafayette: "蘇西・拉法葉",
+    Vulture_Sam: "禿鷹山姆",
+    Willy_the_Kid: "威利小子",
 
     /** Cards */
     Bang: "砰！",
