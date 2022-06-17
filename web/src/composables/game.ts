1bcea7884b1a04f9e0c1b680dc1a3dff8bcec6fe
JacobLinCool <jacoblincool@gmail.com>
2022-06-17 17:20:07 +0800

    feat: client duplicate action protection

--- web/src/composables/game.ts
+++ web/src/composables/game.ts
@@ -3,19 +3,21 @@ import { WSClient } from "./websocket";
 
 export const ws = WSClient();
 
-export function send(action: string, payload: any) {
-    ws.send(JSON.stringify({ action, payload }));
-}
-
-export const state = ref(0);
+export const state = ref(-1);
+export const waiting = ref(false);
 export const game: any = reactive({});
 export const players: string[] = reactive([]);
 export const logs: { type: "chat" | "error"; message: string }[] = reactive([]);
+
+ws.addEventListener("open", () => {
+    state.value = 0;
+});
 
 ws.addEventListener("message", (event) => {
     const message: { type: string; payload: any } = JSON.parse(event.data);
 
     console.log(message);
+    waiting.value = false;
 
     switch (message.type) {
         case "chat":
@@ -30,10 +32,24 @@ ws.addEventListener("message", (event) => {
             players.splice(0, players.length, ...message.payload.players);
             break;
 
+        case "start":
+            game.value = message.payload.game;
+            state.value = 1;
+            break;
+
         default:
             break;
     }
 });
 
+export function send(action: string, payload: any) {
+    if (action !== "keep_alive") {
+        waiting.value = true;
+    }
+    ws.send(JSON.stringify({ action, payload }));
+}
+
 // @ts-ignore For testing purposes
 window.send = send;
+// @ts-ignore For testing purposes
+window.game = game;
