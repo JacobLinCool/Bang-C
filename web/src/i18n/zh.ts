d41680aeed87df2beb23691642c2f8dab45d9b8f
JacobLinCool <jacoblincool@gmail.com>
2022-06-17 21:30:45 +0800

    feat(client): i18n

---
+++ web/src/i18n/zh.ts
@@ -0,0 +1,52 @@
+/**
+ * Chinease Display Names
+ */
+export default {
+    /** Roles */
+    Sheriff: "警長",
+    Deputy: "副警長",
+    Criminal: "罪犯",
+    Traitor: "叛徒",
+
+    /** Characters */
+    Bart_Cassidy: "Bart Cassidy",
+    Black_Jack: "Black Jack",
+    Calamity_Janet: "Calamity Janet",
+    El_Gringo: "El Gringo",
+    Jesse_Jones: "Jesse Jones",
+    Jourdonnais: "Jourdonnais",
+    Kit_Carlson: "Kit Carlson",
+    Lucky_Duke: "Lucky Duke",
+    Paul_Regret: "Paul Regret",
+    Pedro_Ramirez: "Pedro Ramirez",
+    Rose_Doolan: "Rose Doolan",
+    Sid_Ketchum: "Sid Ketchum",
+    Slab_the_Killer: "Slab the Killer",
+    Suzy_Lafayette: "Suzy Lafayette",
+    Vulture_Sam: "Vulture Sam",
+    Willy_the_Kid: "Willy the Kid",
+
+    /** Cards */
+    Bang: "砰！",
+    Missed: "失手！",
+    Gatling: "格林機關槍",
+    Indians: "印第安人！",
+    Panic: "驚慌！",
+    Cat_Balou: "凱特・巴洛",
+    Stagecoach: "驛馬車",
+    Wells_Fargo: "威爾斯・法歌",
+    General_Store: "雜貨店",
+    Beer: "啤酒",
+    Saloon: "酒館",
+    Duel: "決鬥",
+    Barrel: "啤酒桶",
+    Scope: "望遠鏡瞄準器",
+    Mustang: "野馬",
+    Jail: "監獄",
+    Dynamite: "炸藥",
+    Volcanic: "連發槍",
+    Schofield: "史考菲爾德",
+    Remington: "雷明頓",
+    Rev_Carabine: "卡賓槍",
+    Winchester: "溫徹斯特",
+};
