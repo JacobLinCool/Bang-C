4125a8de2083bd3378abb5d6ef805f838bef4d87
JacobLinCool <jacoblincool@gmail.com>
2022-06-17 17:19:44 +0800

    feat: GameBoard skeleton

---
+++ web/src/components/GameBoard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { game, ws, send } from "../composables/game";
+</script>
+
+<template>
+    <div class="w-full h-full"></div>
+</template>
+
+<style scoped></style>
