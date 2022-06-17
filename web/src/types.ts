daf41ca5ce6049a166f9a3d0056658b29398d06a
JacobLinCool <jacoblincool@gmail.com>
2022-06-17 21:32:20 +0800

    feat(client): types

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,22 +1,80 @@
+export enum RoleType {
+    ROLE_LOW_BOUND,
+    Sheriff,
+    Deputy,
+    Criminal,
+    Traitor,
+    ROLE_HIGH_BOUND,
+}
+
+export enum CharacterType {
+    CHARACTER_LOW_BOUND,
+    Bart_Cassidy,
+    Black_Jack,
+    Calamity_Janet,
+    El_Gringo,
+    Jesse_Jones,
+    Jourdonnais,
+    Kit_Carlson,
+    Lucky_Duke,
+    Paul_Regret,
+    Pedro_Ramirez,
+    Rose_Doolan,
+    Sid_Ketchum,
+    Slab_the_Killer,
+    Suzy_Lafayette,
+    Vulture_Sam,
+    Willy_the_Kid,
+    CHARACTER_HIGH_BOUND,
+}
+
+export enum CardType {
+    CARD_LOW_BOUND,
+    Bang,
+    Missed,
+    Gatling,
+    Indians,
+    Panic,
+    Cat_Balou,
+    Stagecoach,
+    Wells_Fargo,
+    General_Store,
+    Beer,
+    Saloon,
+    Duel,
+    Barrel,
+    Scope,
+    Mustang,
+    Jail,
+    Dynamite,
+    Volcanic,
+    Schofield,
+    Remington,
+    Rev_Carabine,
+    Winchester,
+    CARD_HIGH_BOUND,
+}
+
 export interface Card {
-    pointer: number;
-    id: number;
+    /** Offset of address  */
+    x: number;
+    /** Card type, enum (number) */
+    type: CardType;
+    /** SUIT * 100 + VALUE */
     priority: number;
 }
 
 export interface Player {
     name: string;
     id: number;
-    bullet: number;
     hp: number;
-    dead: boolean;
-    role: number;
-    character: number;
-    weapon: Card;
-    barrel: Card;
-    mustang: Card;
-    scope: Card;
-    jail: Card;
-    dynamite: Card;
+    role: RoleType;
+    character: CharacterType;
+    weapon: boolean;
+    barrel: boolean;
+    mustang: boolean;
+    scope: boolean;
+    jail: boolean;
+    dynamite: boolean;
     hands: Card[];
 }
