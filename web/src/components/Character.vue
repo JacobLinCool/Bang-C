b55b0665428ecb255a72717f9af5b51f1272268a
JacobLinCool <jacoblincool@gmail.com>
2022-06-18 18:10:22 +0800

    feat(client): decks, others' positions, spotlight

--- web/src/components/Character.vue
+++ web/src/components/Character.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
-import { CharacterType } from "../types";
+import { Card, CharacterType, RoleType } from "../types";
 import { t } from "../i18n";
+import { Ref, inject } from "vue";
 const props = defineProps({
     type: { type: Number, default: 0 },
 });
+
+const spotlight: Ref<Card | CharacterType | RoleType> | undefined = inject("spotlight");
+const spotlight_type: Ref<number> | undefined = inject("spotlight_type");
 </script>
 
 <template>
     <div
         :title="t(CharacterType[props.type])"
-        class="transition-all hover:z-10 transform hover:scale-180 hover:-translate-y-10"
+        class="transition-all hover:z-20 overflow-hidden rounded-lg group"
+        style="aspect-ratio: 9 / 16"
+        @contextmenu="
+            (event) => {
+                event.preventDefault();
+                spotlight = props.type;
+                spotlight_type = 1;
+            }
+        "
     >
         <img
-            class="w-full"
+            class="transition-all w-full transform group-hover:scale-105"
             style="aspect-ratio: 9 / 16"
             :src="`/character/${CharacterType[props.type]}.png`"
         />
--- web/src/components/Role.vue
+++ web/src/components/Role.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
-import { RoleType } from "../types";
+import { Card, CharacterType, RoleType } from "../types";
 import { t } from "../i18n";
+import { Ref, inject } from "vue";
 const props = defineProps({
     type: { type: Number, default: 0 },
 });
+
+const spotlight: Ref<Card | CharacterType | RoleType> | undefined = inject("spotlight");
+const spotlight_type: Ref<number> | undefined = inject("spotlight_type");
 </script>
 
 <template>
-    <div :title="t(RoleType[props.type])" class="hover:z-10">
+    <div
+        :title="t(RoleType[props.type])"
+        class="hover:z-20 overflow-hidden rounded-lg group"
+        style="aspect-ratio: 9 / 16"
+        @contextmenu="
+            (event) => {
+                event.preventDefault();
+                spotlight = props.type;
+                spotlight_type = 2;
+            }
+        "
+    >
         <img
-            class="w-full"
+            class="transition-all w-full transform group-hover:scale-105"
             style="aspect-ratio: 9 / 16"
             :src="props.type ? `/role/${RoleType[props.type]}.png` : `/card/Card_Back.png`"
         />
