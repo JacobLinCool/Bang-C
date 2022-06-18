8eca0722ec84db5bf535fc696142d3bbb967a0a9
JacobLinCool <jacoblincool@gmail.com>
2022-06-19 00:23:59 +0800

    feat(client): select player & select card

--- web/src/components/Field.vue
+++ web/src/components/Field.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { send, game, choosing } from "../composables/game";
 import Card from "./Card.vue";
 import Character from "./Character.vue";
 import Role from "./Role.vue";
@@ -7,13 +8,24 @@ import { Player } from "../types";
 
 const props = defineProps<{ player: Partial<Player>; self: boolean }>();
 
-function select_card(x: number) {
-    console.log("select card", { x });
+function select_player() {
+    if (choosing.value) {
+        choosing.value = false;
+        send("select_player", { player: props.player.id });
+    }
 }
 </script>
 
 <template>
-    <div class="p-2 rounded-md" style="aspect-ratio: 16 / 9">
+    <div
+        :class="[
+            'transition-all p-2 rounded-md',
+            (game.turn || 0) % (game.players?.length || 10) === props.player.id
+                ? 'bg-orange-300/20 border border-orange-300'
+                : 'bg-blue-300/20',
+        ]"
+        style="aspect-ratio: 16 / 9"
+    >
         <div
             :class="['absolute w-2/3 flex p-1 h-2/3 justify-center', self ? 'bottom-0' : 'top-0']"
             :style="{ right: ((props.player.hands?.length || 0) - 3) * 4 + '%' }"
@@ -27,23 +39,54 @@ function select_card(x: number) {
                         'transform hover:scale-110 hover:z-20 h-full',
                         self ? 'hover:-translate-y-5 z-10' : 'hover:translate-y-4',
                     ]"
-                    :style="{ 'margin-right': ((props.player.hands?.length || 0) - 3) * -7 + '%' }"
-                    @click="select_card(card.x)"
+                    :style="{
+                        'margin-right': ((props.player.hands?.length || 0) - 3) * -5.5 + '%',
+                        left: ((props.player.hands?.length || 0) - 3) * 2 + '%',
+                    }"
                 />
             </ListTransition>
         </div>
-        <div :class="['absolute right-0 w-2/3 h-2/3 flex', self ? 'top-0' : 'bottom-0']">
-            <Card v-if="props.player.weapon" :card="props.player.weapon" class="h-full"></Card>
-            <Card v-if="props.player.scope" :card="props.player.scope" class="h-full"></Card>
-            <Card v-if="props.player.barrel" :card="props.player.barrel" class="h-full"></Card>
-            <Card v-if="props.player.mustang" :card="props.player.mustang" class="h-full"></Card>
-            <Card v-if="props.player.jail" :card="props.player.jail" class="h-full"></Card>
-            <Card v-if="props.player.dynamite" :card="props.player.dynamite" class="h-full"></Card>
+        <div :class="['absolute right-0 w-2/3 h-2/3 flex p-1', self ? 'top-0' : 'bottom-0']">
+            <Card
+                v-if="props.player.weapon"
+                :card="props.player.weapon"
+                class="h-full hover:z-20"
+            ></Card>
+            <Card
+                v-if="props.player.scope"
+                :card="props.player.scope"
+                class="h-full hover:z-20"
+            ></Card>
+            <Card
+                v-if="props.player.barrel"
+                :card="props.player.barrel"
+                class="h-full hover:z-20"
+            ></Card>
+            <Card
+                v-if="props.player.mustang"
+                :card="props.player.mustang"
+                class="h-full hover:z-20"
+            ></Card>
+            <Card
+                v-if="props.player.jail"
+                :card="props.player.jail"
+                class="h-full hover:z-20"
+            ></Card>
+            <Card
+                v-if="props.player.dynamite"
+                :card="props.player.dynamite"
+                class="h-full hover:z-20"
+            ></Card>
         </div>
-        <Role class="w-1/5 p-1" :type="props.player.role"></Role>
-        <Character :class="['w-1/5 p-1']" :type="props.player.character"></Character>
+        <Role @click="select_player" class="w-1/5 p-1" :type="props.player.role"></Role>
+        <Character
+            @click="select_player"
+            :class="['w-1/5 p-1']"
+            :type="props.player.character"
+        ></Character>
 
         <div class="text-xl text-red-500">
+            <span class="text-black text-sm"> {{ props.player.name }} </span> <br />
             <span v-for="i in props.player.hp">♥</span>
         </div>
     </div>
