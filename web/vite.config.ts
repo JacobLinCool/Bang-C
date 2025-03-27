b9641ca64641b0e4eb29b965399efa3cfff27f38
Jodi Kalin <145249665+SEEuHer@users.noreply.github.com>
2025-03-27 05:02:07 -0400

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,1 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), WindiCSS()],
-    server: {
-        proxy: {
-            "/ws": "http://localhost:8080",
-        },
-    },
-});
+Jodi kalin
